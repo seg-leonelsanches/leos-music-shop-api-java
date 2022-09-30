06d2760a6caa65b006fcc4ea01a047fe0746109c
Leonel Sanches <113376080+seg-leonelsanches@users.noreply.github.com>
2022-09-30 15:41:35 -0700

    POST endpoint for drums. Using Model Mapper to convert DTO info into a model.

--- src/main/java/br/com/segment/leosmusicstoreapi/controllers/DrumsController.java
+++ src/main/java/br/com/segment/leosmusicstoreapi/controllers/DrumsController.java
@@ -1,12 +1,14 @@
 package br.com.segment.leosmusicstoreapi.controllers;
 
+import br.com.segment.leosmusicstoreapi.dtos.DrumPostDto;
+import br.com.segment.leosmusicstoreapi.models.Drumkit;
 import br.com.segment.leosmusicstoreapi.repositories.DrumkitRepository;
+import com.sun.istack.NotNull;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/drums")
@@ -15,8 +17,23 @@ public class DrumsController {
     @Autowired
     private DrumkitRepository drumkitRepository;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     @GetMapping("")
     public ResponseEntity<?> getAllDrums() {
         return new ResponseEntity<>(drumkitRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getOneDrum(@PathVariable @NotNull Long id) {
+        return new ResponseEntity<>(drumkitRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<?> addDrum(@RequestBody DrumPostDto drumPostDto) {
+        Drumkit drumkit = modelMapper.map(drumPostDto, Drumkit.class);
+        drumkitRepository.save(drumkit);
+        return new ResponseEntity<>(drumkit, HttpStatus.CREATED);
     }
 }
